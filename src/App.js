9edc893597065d81da1051063657d12b13c19b45
Jeff <jlui820@gmail.com>
2020-09-27 10:57:19 -0700

    setting up project with home page

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Air BnB clone</h1>
+
+    {/* Home */}
+          {/* Header */}
+
+          {/* Banner */}
+
+          {/* Cards */}
+
+          {/* Footer */}
+
+    {/* Search Page */}
+
+        {/* ... */}
+    </div>
+  );
+}
+
+export default App;
