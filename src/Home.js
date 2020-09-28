e512a2ab88d5f6813130b1507c2ef848cc57d7fc
Jeff <jlui820@gmail.com>
2020-09-27 18:07:59 -0700

    add banner component

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Home.css';
+import Banner from './Banner';
 
 const Home = () => {
   return (
     <div className='home'>
-
+      <Banner />
     </div>
   )
 }
