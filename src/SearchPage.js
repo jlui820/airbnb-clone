44d2cfe20e4df9093487c3616e97a7e8fa61edaa
Jeff <jlui820@gmail.com>
2020-10-12 12:28:24 -0700

    add buttons to search page

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,17 +1,21 @@
+import { Button } from '@material-ui/core';
 import React from 'react';
 import './SearchPage.css';
 
 const SearchPage = () => {
   return (
     <div className='searchPage'>
-      <div className="searchPage__info">
-        <p>62 stays 26 august to 30
-          august 2 guest
-        </p>
+      <div className='searchPage__info'>
+        <p>62 stays 26 august to 30 august 2 guest</p>
         <h1>stays nearby</h1>
+        <Button variant='outlined'>Cancellation Flexibility</Button>
+        <Button variant='outlined'>Type of place</Button>
+        <Button variant='outlined'>Price</Button>
+        <Button variant='outlined'>Rooms and beds</Button>
+        <Button variant='outlined'>More filters</Button>
       </div>
     </div>
-  )
+  );
 }
 
 export default SearchPage
